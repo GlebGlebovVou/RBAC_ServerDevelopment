e2ffd07d774c0daf6a12be13d40403ef8814eecc
GlebGlebov <merlinskiy@yandex.ru>
2026-02-16 01:58:33 +0700

    made AbstractRoleAssignment class and fixed bug in AssignmentMetadata

--- src/main/java/AssignmentMetadata.java
+++ src/main/java/AssignmentMetadata.java
@@ -1,6 +1,10 @@
+
+import java.time.LocalDateTime;
+
 public record AssignmentMetadata(String assignedBy, String assignedAt, String reason) {
     static AssignmentMetadata now(String assignedBy, String reason) {
-        return new AssignmentMetadata(assignedBy,"",reason);
+        LocalDateTime date = LocalDateTime.now();
+        return new AssignmentMetadata(assignedBy,date.toString(),reason);
     }
     public String format() {
         return String.format("%s by %s: %s",assignedAt,assignedBy,reason);
