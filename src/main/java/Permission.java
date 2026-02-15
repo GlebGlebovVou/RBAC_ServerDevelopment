ca02405fa749c0621994a3ac1180a7afe709babb
GlebGlebov <merlinskiy@yandex.ru>
2026-02-16 00:26:57 +0700

    Create permission record

---
+++ src/main/java/Permission.java
@@ -0,0 +1,13 @@
+public record Permission(String name, String resource, String description) {
+    public Permission(String name, String resource, String description) {
+        this.name = name.toLowerCase().replaceAll(" ", "");
+        this.description = description.isEmpty() ? "-" : description;
+        this.resource = resource.toLowerCase();
+    }
+    String format() {
+        return String.format("%s on %s: %s",this.name,this.resource,this.description);
+    }
+    boolean matches(String namePattern, String resourcePattern) {
+        return name.matches(namePattern) && resource.matches(resourcePattern)
+    }
+}
