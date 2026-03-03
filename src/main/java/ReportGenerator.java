9dbd038d0c5757f21d472253ec054a6d09fec56b
GlebGlebov <merlinskiy@yandex.ru>
2026-03-03 12:54:41 +0700

    feature/utils: create ReportGenerator

---
+++ src/main/java/ReportGenerator.java
@@ -0,0 +1,79 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ReportGenerator {
+    String generateUserReport(UserManager userManager, AssignmentManager assignmentManager) {
+        StringBuilder f = new StringBuilder();
+        for(User x : userManager.findAll()) {
+            f.append(x.format());
+            f.append("\nRoles: ");
+            List<RoleAssignment> a = assignmentManager.findByUser(x);
+            for(RoleAssignment r : a) {
+                f.append(r.role().format());
+                f.append("\n");
+            }
+            f.append("\n");
+        }
+        return f.toString();
+    }
+    String generateRoleReport(RoleManager roleManager, AssignmentManager assignmentManager) {
+        StringBuilder f = new StringBuilder();
+        for(Role x : roleManager.findAll()) {
+            f.append(x.format());
+            List<RoleAssignment> b = assignmentManager.findByRole(x);
+            List<User> u = new ArrayList<User>();
+            int count = 0;
+            for(RoleAssignment a : b) {
+                if(!u.contains(a.user())) {
+                    u.add(a.user());
+                    count += 1;
+                }
+            }
+            f.append(String.format("\namount of users: %d\n\n",count));
+        }
+        return f.toString();
+    }
+    String generatePermissionMatrix(UserManager userManager, AssignmentManager assignmentManager) {
+        StringBuilder f = new StringBuilder();
+        f.append("\t");
+        HashSet<String> resources = new HashSet<String>();
+        List<Role> roles = new ArrayList<Role>();
+        for(RoleAssignment a : assignmentManager.findAll()) {
+            if (!roles.contains(a.role())) {
+                for (Permission g : a.role().getPermissions()) {
+                    if(!resources.contains(g.resource())) {
+                        f.append(String.format("%s\t",g.resource()));
+                    }
+                    resources.add(g.resource());
+                }
+                roles.add(a.role());
+            }
+        }
+        for(User i : userManager.findAll()) {
+            f.append("\n");
+            HashSet<String> res = new HashSet<String>();
+            List<RoleAssignment> r  = assignmentManager.findByUser(i);
+            for(RoleAssignment u : r) {
+                for(Permission o : u.role().getPermissions()) {
+                    res.add(o.resource());
+                }
+            }
+            for(String l : resources) {
+                f.append(res.contains(l) ? "+\t" : "-\t");
+            }
+        }
+        return f.toString();
+    }
+    void exportToFile(String report, String filename) throws IOException {
+        try(FileWriter w = new FileWriter(filename)) {
+            w.write(report);
+        }
+        catch(IOException e) {
+            throw new IOException();
+        }
+    }
+}
