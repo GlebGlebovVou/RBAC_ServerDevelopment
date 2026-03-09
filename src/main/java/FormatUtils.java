8b9f64d6eba7728a7866d8af9c242e316d0eb935
GlebGlebovVou <merlsinkiy@yandex.ru>
2026-03-09 14:50:43 +0700

    feature/utils: create FormatUtils

---
+++ src/main/java/FormatUtils.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public class FormatUtils {
+    String formatTable(String[] headers, List<String[]> rows) {
+        StringBuilder f = new StringBuilder();
+        int size = 10 * headers.length;
+        f.append(String.format("%s\n","-".repeat(Math.max(size,0))));
+        for(String i : headers) {
+            f.append(String.format("*%s",(padRight(truncate(i,10),10))));
+        }
+        f.append("*\n|");
+        for(String[] i : rows) {
+            for(String j : i) {
+                f.append(String.format("|%s",padRight(truncate(j,10),10)));
+            }
+            f.append("|*|");
+        }
+        return f.toString();
+    }
+    String formatBox(String text) {
+        return String.format(
+                "%s\n|%s|\n%s",
+                "-".repeat(text.length() + 2),
+                text,
+                "-".repeat(text.length() + 2));
+    }
+    String formatHeader(String text) {
+        return String.format(
+                "%s\n*%s*\n%s",
+                "*".repeat(text.length() + 2),
+                text,
+                "*".repeat(text.length() + 2));
+    }
+    String truncate(String text, int maxLength) {
+        if(text.length() > maxLength) {
+            char[] new_text = new char[maxLength];
+            text.getChars(0,maxLength-2,new_text,0);
+            return String.copyValueOf(new_text).concat("...");
+        }
+        return text;
+    }
+    String padRight(String text, int length) {
+        return text.concat(" ".repeat(Math.max(0,length - text.length())));
+    }
+    String padLeft(String text, int length) {
+        return " ".repeat(Math.max(0,length - text.length())).concat(text);
+    }
+}
